77cc3b1f025bac8763f80f3a19486d24d7e0d801
Nahuel Salomon <nahuelsalomon@MacBook-Air-de-Nahuel.local>
2022-08-17 20:48:38 +0200

    creacion de avBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NatbVar = () => {
+const NavBar = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const NatbVar = () => {
   )
 }
 
-export default NatbVar
+export default NavBar
