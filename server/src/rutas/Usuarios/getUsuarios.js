a7fae87e0b85dc1d342bcc040084eb3cc2b42e6c
Cristian Delvalle <criatiaan.delva@gmail.com>
2022-08-23 20:46:42 -0300

    busqueda por nombre ruta getUsuario

--- server/src/rutas/Usuarios/getUsuarios.js
+++ server/src/rutas/Usuarios/getUsuarios.js
@@ -4,8 +4,18 @@ const Usuarios = require('../../modelos/Usuarios')
 const getUsuarios = Router()
 
 getUsuarios.get('/', async (req,res,next) =>{
-    const usuarios = await Usuarios.find()
-    res.send(usuarios)
+
+    try{    const {nombre} = req.query
+    if(nombre) {
+        const usuarios = await Usuarios.find()
+        const usuariosBuscados = usuarios.filter(e=>e.firstName.includes(nombre.toLocaleLowerCase()))
+        res.send(usuariosBuscados.length?usuariosBuscados:"Ese nombre no coincide D:")
+    }else{
+    res.send( await Usuarios.find())
+}}
+    catch(error){
+        console.error({message:error.message})
+    }
 })
 
 module.exports = getUsuarios
