30f6ebc24c6e36c50053a33ee731365db46c2df1
Samir José <samirjose675@gmail.com>
2022-09-02 10:39:38 -0300

    Hide google credentials

--- server/middlewares/google.js
+++ server/middlewares/google.js
@@ -1,12 +1,14 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require("../src/modelos/Usuarios")
+var dotenv = require("dotenv")
+dotenv.config()
 
 const emails = ["samirjose675@gmail.com"]
 
 passport.use("auth-google", new GoogleStrategy({
-    clientID: "670818911101-b0rnhun1enn4kvisuk4a1eh6cr6pn3qn.apps.googleusercontent.com",
-    clientSecret: "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",
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'http://localhost:4000/autenticar/google'
   },
   async function (accessToken, refreshToken, profile, done) {
