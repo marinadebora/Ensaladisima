458e6bef52d64c102a64678c97719a77fc2bda83
Debora <m_deby_c@hotmail.com>
2022-09-02 13:19:26 -0300

    warnings

--- client/src/components/Ensaladas.jsx
+++ client/src/components/Ensaladas.jsx
@@ -12,12 +12,12 @@ const Ensaladas = ({image, name, base, protein, complement, sauce, topping}) => 
       <div>
         <img src={image} alt="img" id="ensaladaImagen" />
         <div class="row" id="addContent">
-          <a class="col-3" id="textButtonAdd">Mediana</a>
+          <a href=' ' class="col-3" id="textButtonAdd">Mediana</a>
           <button class="col-1" id="buttonAddM">
           <i class="bi bi-plus-circle-fill"></i>
           </button>
 
-          <a class="col-3" id="textButtonAdd">Grande</a>
+          <a href=' ' class="col-3" id="textButtonAdd">Grande</a>
           <button class="col-1" id="buttonAddG">
           <i class="bi bi-plus-circle-fill"></i>
           </button>
@@ -26,11 +26,11 @@ const Ensaladas = ({image, name, base, protein, complement, sauce, topping}) => 
       </div>
           <div id="ingredientes">
           <ul id="ingredientesUl">
-            <span id="numberLi">1</span><a id="tipoIngrediente">Base: </a> {base?.map(e => <li id="ingredientesLi"> {e}</li>)} 
-            <span id="numberLi">2</span><a id="tipoIngrediente">Proteina: </a> {protein?.map(e => <li id="ingredientesLi"> {e}</li>)}
-            <span id="numberLi">3</span><a id="tipoIngrediente">Complementos: </a> {complement?.map(e => <li id="ingredientesLi"> {e}</li>)}
-            <span id="numberLi">4</span><a id="tipoIngrediente">Salsas: </a> {sauce?.map(e => <li id="ingredientesLi"> {e}</li>)}
-            <span id="numberLi">5</span><a id="tipoIngrediente">Topping: </a> {topping?.map(e => <li id="ingredientesLi"> {e}</li>)}
+            <span id="numberLi">1</span><a  href=' 'id="tipoIngrediente">Base: </a> {base?.map(e => <li id="ingredientesLi"> {e}</li>)} 
+            <span id="numberLi">2</span><a href=' 'id="tipoIngrediente">Proteina: </a> {protein?.map(e => <li id="ingredientesLi"> {e}</li>)}
+            <span id="numberLi">3</span><a href=' ' id="tipoIngrediente">Complementos: </a> {complement?.map(e => <li id="ingredientesLi"> {e}</li>)}
+            <span id="numberLi">4</span><a href=' 'id="tipoIngrediente">Salsas: </a> {sauce?.map(e => <li id="ingredientesLi"> {e}</li>)}
+            <span id="numberLi">5</span><a href=' 'id="tipoIngrediente">Topping: </a> {topping?.map(e => <li id="ingredientesLi"> {e}</li>)}
           </ul>
           </div>
       </div>
