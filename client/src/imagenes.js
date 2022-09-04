eb80c2c120adef200bf0603a8b488f94513148bd
Debora <m_deby_c@hotmail.com>
2022-09-03 21:05:38 -0300

    carrousel

---
+++ client/src/imagenes.js
@@ -0,0 +1,11 @@
+export const imag={
+   image1: 'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241659/ensaladas%20para%20carrousel/ensalada_8_pqf2vd.jpg',
+   image2:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241677/ensaladas%20para%20carrousel/ensalada_7_c3mwkp.jpg',
+   image3:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241682/ensaladas%20para%20carrousel/ensalada_6_uqltpl.jpg',
+   image4:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241684/ensaladas%20para%20carrousel/ensalada_5_xrzhwe.jpg',
+   image5:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241686/ensaladas%20para%20carrousel/ensalada_4_o0bsvt.jpg',
+   image6:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241723/ensaladas%20para%20carrousel/ensalada_1_qvkjsj.jpg',
+   image7:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241729/ensaladas%20para%20carrousel/ensalada_2_gxwwy4.jpg',
+   image8:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241739/ensaladas%20para%20carrousel/ensalada_3_aqsisw.jpg',
+   image9:'https://res.cloudinary.com/deqbqghhq/image/upload/v1662241758/ensaladas%20para%20carrousel/ensalada_wz499c.jpg'
+}
