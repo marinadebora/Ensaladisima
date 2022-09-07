a317e272d048be8ddf64e380df476f81fca5a860
AlexisRafael <alexis24450@gmail.com>
2022-09-07 09:51:52 -0500

    secrean las rutas /mediana y /grande

---
+++ server/src/rutas/EnsaladasBigs/postEnsaladasBig.js
@@ -0,0 +1,21 @@
+const {Router}=require('express')
+const EnsaladasBig = require('../../modelos/EnsaladasBig')
+
+const postEnsaladaGrande = Router()
+
+postEnsaladaGrande.post('/', async (req,res,next) =>{
+    const {base,complement,protein,suace,topping} = req.body
+    try {
+        const buscar = await EnsaladasBig.find({base,complement,protein,suace,topping})
+        if(buscar[0]){
+            res.send(buscar[0])
+        }else{
+            const crear = await EnsaladasBig.create({base,complement,protein,suace,topping})
+            res.send(crear)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = postEnsaladaGrande
