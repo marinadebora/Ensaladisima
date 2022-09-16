ec7403a6f908878c86f573b036c76d0ffe2fb389
AlexisRafael <alexis24450@gmail.com>
2022-09-16 10:07:31 -0500

    se solucionan problemas con google

--- server/src/rutas/Usuarios/Midelwer/crearPedidoMidelwere.js
+++ server/src/rutas/Usuarios/Midelwer/crearPedidoMidelwere.js
@@ -4,13 +4,16 @@ const Pedidos = require('../../../modelos/Pedidos')
 const crearPedidoMidelwere = async (req,res,next)=>{
     const { email, password, saladsMenu, saladsMenuBig, saladsMed, saladsBig, beverages, desserts ,google} = req.body;
     try {
-        const buscar = await Usuarios.find({email})
+        console.log(await Usuarios.find())
+        const buscar = await Usuarios.find({email:email})
+        console.log(buscar[0])
         console.log(!buscar[0]?.orders[0])
         if(!buscar[0]?.orders[0]){
             const crear = await Pedidos.create({user:buscar[0]?._id})
-            const actualizar = await Usuarios.findOneAndUpdate({email},{
+            const actualizar = await Usuarios.findOneAndUpdate({_id: buscar[0]?._id},{
             orders:crear?._id
             })
+            console.log(actualizar)
             next()
         }else{
             next()
