7cb041878440f28adfbf15849178465cad7696a8
Debora <m_deby_c@hotmail.com>
2022-09-19 20:21:42 -0300

    WARNING

--- client/src/components/CarrouselEP.jsx
+++ client/src/components/CarrouselEP.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { historialCompra, modificarPedido } from '../action/'
 import { imag } from '../imagenes';
