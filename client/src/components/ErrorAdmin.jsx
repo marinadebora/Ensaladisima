19a85c306e16e7b27366a5bb6fc9e064e711c412
Debora <m_deby_c@hotmail.com>
2022-09-19 08:28:55 -0300

    historial de compras usuario

--- client/src/components/ErrorAdmin.jsx
+++ client/src/components/ErrorAdmin.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import '../styles/Error.css'
 export const ErrorAdmin = () => {
   return (
-  <div>
-<img src="https://res.cloudinary.com/deqbqghhq/image/upload/v1663264774/samples/no_pasar_qfttdj.jpg" alt="permiso denegado" />
+  <div id="ErrorContainer">
+<img id="ErrorId" src="https://res.cloudinary.com/deqbqghhq/image/upload/v1663264774/samples/no_pasar_qfttdj.jpg" alt="permiso denegado" />
   </div>
   )
 };
