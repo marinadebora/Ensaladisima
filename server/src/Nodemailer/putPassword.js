8214e011f283833b752bb3d0cc72228d9f63cdaa
AlexisRafael <alexis24450@gmail.com>
2022-09-21 19:53:13 -0500

    listo

--- server/src/Nodemailer/putPassword.js
+++ server/src/Nodemailer/putPassword.js
@@ -5,6 +5,7 @@ const { transporter } = require("./postEmail");
 const correoPassword = async (req,res )=>{
     const {email} = req.body;
     try {
+        
         const correo= await Usuarios.find({email:email})
         await transporter.sendMail({
             from: '"CAMBIO DE PASSWORD" <ensaladisima02@gmail.com', // sender address
